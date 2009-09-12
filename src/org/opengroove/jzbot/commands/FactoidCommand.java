664def8baa71645d8016c3a7334fa33f507131c3
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-09-12 09:16:16 +0000

    Added support for "~factoid replace"
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@528 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/org/opengroove/jzbot/commands/FactoidCommand.java
+++ src/org/opengroove/jzbot/commands/FactoidCommand.java
@@ -17,7 +17,7 @@ public class FactoidCommand implements Command
     }
     
     public void run(String channel, boolean pm, String sender, String hostname,
-        String arguments)
+            String arguments)
     {
         boolean isGlobal = false;
         if (arguments.startsWith("global "))
@@ -27,15 +27,16 @@ public class FactoidCommand implements Command
         }
         String[] argumentsTokenized1 = arguments.split(" ", 2);
         String command = argumentsTokenized1[0];
-        if ((!isGlobal) && (channel == null) && (!command.equalsIgnoreCase("isglobal")))
+        if ((!isGlobal) && (channel == null)
+                && (!command.equalsIgnoreCase("isglobal")))
         {
             JZBot.bot.sendMessage(pm ? sender : channel,
-                "For non-global commands, you must specify "
-                    + "a channel (unless it is the isglobal command)");
+                    "For non-global commands, you must specify "
+                            + "a channel (unless it is the isglobal command)");
             return;
         }
-        String afterCommand =
-            (argumentsTokenized1.length > 1) ? argumentsTokenized1[1] : "";
+        String afterCommand = (argumentsTokenized1.length > 1) ? argumentsTokenized1[1]
+                : "";
         /*
          * command is something like create, delete, isglobal, etc., and
          * afterCommand is the rest
@@ -43,24 +44,53 @@ public class FactoidCommand implements Command
         Channel c = null;
         if (!isGlobal)
             c = JZBot.storage.getChannel(channel);
-        if (command.equals("create"))
+        boolean processed = false;
+        if (command.equals("delete") || command.equals("replace"))
         {
+            processed = true;
+            verifyOpSuperop(isGlobal, channel, hostname);
+            String[] argumentsTokenized2 = afterCommand.split(" ", 2);
+            if (argumentsTokenized2.length != 2)
+                throw new ResponseException(
+                        "You need to specify the factoid itself");
+            String factoidName = argumentsTokenized2[0];
+            if (factoidName.equals(""))
+                throw new ResponseException("You need to specify the factoid");
+            Factoid f;
+            if (isGlobal)
+                f = JZBot.storage.getFactoid(factoidName);
+            else
+                f = c.getFactoid(factoidName);
+            if (f == null)
+                throw new ResponseException("That factoid doesn't exist");
+            if (isGlobal)
+                JZBot.storage.getFactoids().remove(f);
+            else
+                c.getFactoids().remove(f);
+            if (command.equals("delete"))
+                JZBot.bot.sendMessage(pm ? sender : channel, "Factoid "
+                        + afterCommand + " deleted.");
+        }
+        if (command.equals("create") || command.equals("replace"))
+        {
+            processed = true;
             verifyOpSuperop(isGlobal, channel, hostname);
             if (afterCommand.equals(""))
                 throw new ResponseException("You need to specify the factoid");
             String[] argumentsTokenized2 = afterCommand.split(" ", 2);
             if (argumentsTokenized2.length != 2)
-                throw new ResponseException("You need to specify the factoid itself");
+                throw new ResponseException(
+                        "You need to specify the factoid itself");
             String factoidName = argumentsTokenized2[0];
             if (JZBot.commands.get(factoidName) != null)
                 throw new ResponseException("That is a reserved keyword.");
             String factoidContents = argumentsTokenized2[1];
             if (c != null && c.getFactoid(factoidName) != null)
                 throw new ResponseException(
-                    "That factoid already exists as a channel-specific factoid");
+                        "That factoid already exists as a channel-specific factoid");
             else if (c == null && JZBot.storage.getFactoid(factoidName) != null)
                 throw new ResponseException(
-                    "That factoid already exists as a global factoid");
+                        "That factoid already exists as a global factoid");
             /*
              * The factoid does not exist. Let's create it.
              */
@@ -73,33 +103,18 @@ public class FactoidCommand implements Command
                 JZBot.storage.getFactoids().add(f);
             else
                 c.getFactoids().add(f);
-            System.out.println("created fact " + factoidName + " " + factoidContents);
-            JZBot.bot.sendMessage(pm ? sender : channel, "Factoid " + factoidName
-                + " created.");
+            System.out.println("created fact " + factoidName + " "
+                    + factoidContents);
+            JZBot.bot.sendMessage(pm ? sender : channel, "Factoid "
+                    + factoidName
+                    + (command.equals("replace") ? "replaced. " : " created."));
         }
-        else if (command.equals("delete"))
+        if (command.equals("list"))
         {
-            verifyOpSuperop(isGlobal, channel, hostname);
-            if (afterCommand.equals(""))
-                throw new ResponseException("You need to specify the factoid");
-            Factoid f;
-            if (isGlobal)
-                f = JZBot.storage.getFactoid(afterCommand);
-            else
-                f = c.getFactoid(afterCommand);
-            if (f == null)
-                throw new ResponseException("That factoid doesn't exist");
-            if (isGlobal)
-                JZBot.storage.getFactoids().remove(f);
-            else
-                c.getFactoids().remove(f);
-            JZBot.bot.sendMessage(pm ? sender : channel, "Factoid " + afterCommand
-                + " deleted.");
-        }
-        else if (command.equals("list"))
-        {
+            processed = true;
             System.out.println("command is list, " + isGlobal);
-            JZBot.bot.sendMessage(pm ? sender : channel, "Start of factoid list");
+            JZBot.bot.sendMessage(pm ? sender : channel,
+                    "Start of factoid list");
             StoredList<Factoid> list;
             if (isGlobal)
                 list = JZBot.storage.getFactoids();
@@ -110,25 +125,29 @@ public class FactoidCommand implements Command
                 String currentList = "";
                 for (Factoid f : list.isolate())
                 {
-                    currentList += f.getName() + "         ";
-                    if (currentList.length() > 250)
+                    currentList += f.getName() + "    ";
+                    if (currentList.length() > 400)
                     {
-                        JZBot.bot.sendMessage(pm ? sender : channel, currentList);
+                        JZBot.bot.sendMessage(pm ? sender : channel,
+                                currentList);
                         currentList = "";
                     }
                 }
                 if (!currentList.equals(""))
                     JZBot.bot.sendMessage(pm ? sender : channel, currentList);
             }
-            JZBot.bot.sendMessage(pm ? sender : channel, "End of factoid list");
-            if (!isGlobal)
-                JZBot.bot.sendMessage(pm ? sender : channel,
-                    "You should also run factoid global list for"
-                        + " global factoids. These were not included "
-                        + "in this list.");
+            JZBot.bot
+                    .sendMessage(
+                            pm ? sender : channel,
+                            "End of factoid list. "
+                                    + (isGlobal ? ""
+                                            : "You should also run factoid global list for"
+                                                    + " global factoids. These were not included "
+                                                    + "in this list."));
         }
-        else if (command.equals("literal"))
+        if (command.equals("literal"))
         {
+            processed = true;
             if (afterCommand.equals(""))
                 throw new ResponseException("You need to specify the factoid");
             Factoid f;
@@ -140,7 +159,12 @@ public class FactoidCommand implements Command
                 throw new ResponseException("That factoid doesn't exist");
             JZBot.bot.sendMessage(pm ? sender : channel, f.getValue());
         }
-        else
+        if (command.equals("info"))
+        {
+            processed = true;
+            throw new ResponseException("Info is not supported yet.");
+        }
+        if (!processed)
         {
             throw new ResponseException("Invalid factoid command");
         }
@@ -154,7 +178,8 @@ public class FactoidCommand implements Command
      * @param channel
      * @param hostname
      */
-    private void verifyOpSuperop(boolean isGlobal, String channel, String hostname)
+    private void verifyOpSuperop(boolean isGlobal, String channel,
+            String hostname)
     {
         if (isGlobal)
             JZBot.bot.verifySuperop(hostname);
