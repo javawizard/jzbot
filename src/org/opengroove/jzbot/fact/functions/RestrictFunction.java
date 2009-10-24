30d2be0ceb19d05ae30f82fe8ec827b08156a2ad
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-10-24 15:07:14 +0000

    Erg
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@708 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/org/opengroove/jzbot/fact/functions/RestrictFunction.java
+++ src/org/opengroove/jzbot/fact/functions/RestrictFunction.java
@@ -4,7 +4,7 @@ import org.opengroove.jzbot.fact.ArgumentList;
 import org.opengroove.jzbot.fact.FactContext;
 import org.opengroove.jzbot.fact.Function;
 
-public class Restrict extends Function
+public class RestrictFunction extends Function
 {
     
     @Override
