489762f58d848e3c054f541968de65df348144a0
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-11-17 19:03:46 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@846 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ src/jw/jzbot/fact/functions/xml/XmladdFunction.java
@@ -0,0 +1,25 @@
+package jw.jzbot.fact.functions.xml;
+
+import jw.jzbot.fact.ArgumentList;
+import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.Function;
+
+public class XmladdFunction extends Function
+{
+    
+    @Override
+    public String evaluate(ArgumentList arguments, FactContext context)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public String getHelp(String topic)
+    {
+        return "Syntax: {{xmladd||<target>||<index>||<tagname>}} -- Adds an XML tag, with "
+                + "the tag name <tagname>, to the target element <target> at the position "
+                + "<index>. 0 is the first element. This then evaluates to ";
+    }
+    
+}
