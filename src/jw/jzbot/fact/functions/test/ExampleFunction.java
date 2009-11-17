060e44247f17f645fcf02e11dc529b6b55a85cde
UndeadEditor <UndeadEditor@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-11-17 19:30:00 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@848 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ src/jw/jzbot/fact/functions/test/ExampleFunction.java
@@ -0,0 +1,21 @@
+package jw.jzbot.fact.functions.test;
+
+import jw.jzbot.fact.ArgumentList;
+import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.Function;
+
+public abstract class ExampleFunction extends Function {
+
+	@Override
+	public String evaluate(ArgumentList arguments, FactContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHelp(String topic) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
