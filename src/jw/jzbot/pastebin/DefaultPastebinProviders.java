aa84e0f40e41fd2fe66fe8e6e1bd89b749421a99
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-11-20 22:59:51 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@869 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/pastebin/DefaultPastebinProviders.java
+++ src/jw/jzbot/pastebin/DefaultPastebinProviders.java
@@ -9,18 +9,23 @@ public class DefaultPastebinProviders
     {
         PastebinService
                 .installProvider(new PDPastebin("pastebin.com", true, "pastebin.php"));
-        PastebinService.installProvider(new PDPastebin("p.baf.cc", false,
-                "pastebin.php"));
+        PastebinService.installProvider(new PDPastebin("p.baf.cc", false, "pastebin.php"));
         PastebinService.installProvider(new PDPastebin("pastebin.flamingspork.com", false,
                 "pastebin.php"));
         PastebinService.installProvider(new PDPastebin("paste.amahi.org", false,
                 "pastebin.php"));
         PastebinService
                 .installProvider(new PDPastebin("ampaste.net", true, "pastebin.php"));
+        PastebinService
+                .installProvider(new PDPastebin("paste.liquidswords.org", false, "pastebin.php"));
         PastebinService.installProvider(new PDPastebin("pastebin.im", false, "index.php"));
+        PastebinService.installProvider(new PDPastebin("paste.uberdragon.net", false,
+                "index.php"));
         // pastebin.flamingspork.com
         // paste.amahi.org
         // p.baf.cc
+        // paste.uberdragon.net
+        // paste.liquidswords.org
         // PastebinService.installProvider(new PastebinDotCa());
     }
 }
