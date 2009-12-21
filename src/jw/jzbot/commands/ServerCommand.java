5d33645cac52e14f045384f4f1a4b5b4659cf695
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-12-21 18:48:58 +0000

    Removed the commands mentioned in the previous commit from the command registration section of JZBot.java, and more work on ~server
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@943 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/commands/ServerCommand.java
+++ src/jw/jzbot/commands/ServerCommand.java
@@ -1,6 +1,7 @@
 package jw.jzbot.commands;
 
 import jw.jzbot.Command;
+import jw.jzbot.JZBot;
 
 public class ServerCommand implements Command
 {
@@ -15,6 +16,17 @@ public class ServerCommand implements Command
     public void run(String server, String channel, boolean pm, String sender,
             String hostname, String arguments)
     {
+        // TODO: make a config var that, when set to a certain value, allows the list of
+        // servers to be read by non-superops, too.
+        JZBot.verifySuperop(server, hostname);
+        if (arguments.equals(""))
+        {
+            JZBot.getServer(server).sendMessage(
+                    pm ? sender : channel,
+                    "You need to specify one of add, delete, activate, "
+                            + "deactivate, or edit.");
+            return;
+        }
     }
     
 }
