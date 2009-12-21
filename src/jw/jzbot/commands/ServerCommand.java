27bb2ccf06f02953eabe49451c458c1d4c218746
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-12-21 18:43:24 +0000

    Deleted ~isrestricted, ~unrestrict, and ~restrict (as they're being merged into ~factoid; the soon-to-be ~scope is also being merged into ~factoid), and started work on ~server
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@942 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/commands/ServerCommand.java
+++ src/jw/jzbot/commands/ServerCommand.java
@@ -8,16 +8,13 @@ public class ServerCommand implements Command
     @Override
     public String getName()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return "server";
     }
     
     @Override
     public void run(String server, String channel, boolean pm, String sender,
             String hostname, String arguments)
     {
-        // TODO Auto-generated method stub
-        
     }
     
 }
