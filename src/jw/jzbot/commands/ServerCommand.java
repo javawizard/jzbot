ab9c25f04367753784d1c13ec6a66803b470d942
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-12-23 08:58:37 +0000

    Implemented more of the ~server command
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@958 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/commands/ServerCommand.java
+++ src/jw/jzbot/commands/ServerCommand.java
@@ -1,11 +1,16 @@
 package jw.jzbot.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jw.jzbot.Command;
+import jw.jzbot.ConnectionContext;
 import jw.jzbot.JZBot;
 import jw.jzbot.Messenger;
 import jw.jzbot.ResponseException;
 import jw.jzbot.ServerUser;
 import jw.jzbot.storage.Server;
+import jw.jzbot.utils.JZUtils;
 
 public class ServerCommand implements Command
 {
@@ -72,7 +77,15 @@ public class ServerCommand implements Command
         }
         else if (subcommand.equals("details"))
         {
-            
+            Server s = JZBot.storage.getServer(serverName);
+            if (s == null)
+                throw new ResponseException(
+                        "There isn't such a server by that name. Try \"server list\" "
+                                + "to get a list of servers, and then do "
+                                + "\"server details <name>\".");
+            source.sendMessage("protocol:" + s.getProtocol() + "  host:" + s.getServer()
+                    + "  port:" + s.getPort() + "  nick:" + s.getNick() + "  password:"
+                    + s.getPassword().replaceAll(".", "*"));
         }
         else if (subcommand.equals("activate"))
         {
@@ -96,15 +109,75 @@ public class ServerCommand implements Command
         }
         else if (subcommand.equals("edit"))
         {
-            
+            Server s = JZBot.storage.getServer(serverName);
+            if (s == null)
+                throw new ResponseException(
+                        "There isn't such a server by that name. Try \"server list\" "
+                                + "to get a list of servers, and then do "
+                                + "\"server edit <name>\".");
+            ConnectionContext realCon = JZBot.getRealConnection(s.getName());
+            if (s.isActive() || (realCon != null && realCon.getConnection().isConnected()))
+                throw new ResponseException(
+                        "You can't edit servers that are connected or activated. "
+                                + "So, when running \"server list\", you can only "
+                                + "edit servers that do not have flags 1 and 3.");
+            if (tokens[0].equals(""))
+                throw new ResponseException("You need to specify the name of the property "
+                        + "to edit, like \"server edit " + serverName
+                        + " <propname> <propvalue>\". Allowed names are "
+                        + "protocol, host, port, nick, and password. Server "
+                        + "name-changing will be supported in the future.");
+            String key = tokens[0];
+            if (tokens.length < 2)
+                throw new ResponseException("You need to specify the new value "
+                        + "for this property, " + "like \"server edit " + serverName + " "
+                        + key + " <newvalue>\".");
+            String value = tokens[1];
+            if (key.equals("protocol"))
+                s.setProtocol(value);
+            else if (key.equals("host"))
+                s.setServer(value);
+            else if (key.equals("port"))
+                s.setPort(Integer.parseInt(value));
+            else if (key.equals("nick"))
+                s.setNick(value);
+            else if (key.equals("password"))
+                s.setPassword(value);
+            else
+                throw new ResponseException("That's not a valid property name. "
+                        + "Try \"server edit " + serverName
+                        + "\" for a list of valid property names.");
+            source.sendMessage("The server was successfully updated.");
         }
         else if (subcommand.equals("list"))
         {
-            
+            source.sendMessage("Here's the list. Servers in this list are in the format "
+                    + "<flags>:<name>, where <flags> are some flags and <name> is the "
+                    + "name of the server. Flags are 1: active, 2: has a connection "
+                    + "object, 3: currently connected, 0: no other flags.");
+            List<String> items = new ArrayList<String>();
+            for (Server s : JZBot.storage.getServers().isolate())
+            {
+                String sName = s.getName();
+                String flags = "";
+                if (s.isActive())
+                    flags += "1";
+                ConnectionContext c = JZBot.getRealConnection(sName);
+                if (c != null)
+                {
+                    flags += "2";
+                    if (c.getConnection().isConnected())
+                        flags += "3";
+                }
+                if (flags.equals(""))
+                    flags = "0";
+                items.add(flags + ":" + sName);
+            }
+            JZUtils.ircSendDelimited(items.toArray(new String[0]), "  ", source);
         }
         else if (subcommand.equals("current"))
         {
-            
+            source.sendMessage("Your current server is " + server + ".");
         }
         else
         {
