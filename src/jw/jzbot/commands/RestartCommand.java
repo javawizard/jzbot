e32b8a9fc0b9bf577022674ec6eee8dcfd7ffec9
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-12-28 22:10:45 +0000

    Removed the delay incurred by the restart command, and removed its reply (as a disconnecting bot is reply enough). I also removed the note stating that it should be moved to a factpack as this could considerably increase the chances of the bot freezing.
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@969 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/commands/RestartCommand.java
+++ src/jw/jzbot/commands/RestartCommand.java
@@ -18,22 +18,7 @@ public class RestartCommand implements Command
     public void run(String server, String channel, boolean pm, ServerUser sender,
             Messenger source, String arguments)
     {
-        // FIXME: this could possibly be moved to a factpack. Maybe keep it just in case,
-        // but allow it to be redefined.
         sender.verifySuperop();
-        source.sendMessage("The bot will be restarted shortly. Note that this only "
-                + "works if you used the jzbot wrapper script to start "
-                + "the bot. If you didn't, the bot will shut down instead.");
-        long sleepDuration = 1000;
-        try
-        {
-            Thread.sleep(sleepDuration);
-        }
-        catch (InterruptedException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
         System.exit(17);
     }
     
