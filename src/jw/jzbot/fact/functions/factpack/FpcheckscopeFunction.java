dde64d2db26a7a9b264edf5f90c2714a9d96032b
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-01-06 00:13:20 +0000

    More porting
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1025 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/fact/functions/factpack/FpcheckscopeFunction.java
+++ src/jw/jzbot/fact/functions/factpack/FpcheckscopeFunction.java
@@ -3,6 +3,8 @@ package jw.jzbot.fact.functions.factpack;
 import jw.jzbot.FactScope;
 import jw.jzbot.fact.ArgumentList;
 import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.FactoidException;
+import jw.jzbot.fact.FactpackInstallationException;
 import jw.jzbot.fact.Function;
 import jw.jzbot.fact.Sink;
 
@@ -13,7 +15,19 @@ public class FpcheckscopeFunction extends Function
     public void evaluate(Sink sink, ArgumentList arguments, FactContext context)
     {
         String scopeName = arguments.resolveString(0);
-        // FactScope scope = FactScope.
+        FactScope scope = FactScope.valueOf(scopeName);
+        String target = context.getLocalVars().get("factpack-target");
+        FactScope actualScope;
+        if (target.contains("#"))
+            actualScope = FactScope.channel;
+        else if (target.contains("@"))
+            actualScope = FactScope.server;
+        else
+            actualScope = FactScope.global;
+        if (actualScope != scope)
+            throw new FactpackInstallationException("That factpack must be installed at "
+                    + scope + " scope, but you tried to install it at " + actualScope
+                    + " scope.");
     }
     
     @Override
