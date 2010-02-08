22e0ce310e43242ce1809b5d66a9954c9b8da9dc
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-02-08 05:30:26 +0000

    Added the {invite} function
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1192 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ src/jw/jzbot/fact/functions/irc/InviteFunction.java
@@ -0,0 +1,27 @@
+package jw.jzbot.fact.functions.irc;
+
+import jw.jzbot.fact.ArgumentList;
+import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.Function;
+import jw.jzbot.fact.Sink;
+
+public class InviteFunction extends Function
+{
+    
+    @Override
+    public void evaluate(Sink sink, ArgumentList arguments, FactContext context)
+    {
+        context.checkedGetConnection().getConnection().sendInvite(
+                arguments.resolveString(0), arguments.resolveString(1));
+    }
+    
+    @Override
+    public String getHelp(String topic)
+    {
+        return "Syntax: {invite|<user>|<channel>} -- Sends an invitation to the "
+            + "specified user to join the specified channel. Most clients require "
+            + "that the bot be an operator at the specified channel in order to "
+            + "send out invites.";
+    }
+    
+}
