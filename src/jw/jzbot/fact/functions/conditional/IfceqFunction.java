529b268ac249e0bf1971b83ba08d820a7b2e34f0
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-02-24 20:49:53 +0000

    Added {ifceq} and {ifcneq}, which are case-sensitive variants of {ifeq} and {ifneq}, respectively.
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1217 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ src/jw/jzbot/fact/functions/conditional/IfceqFunction.java
@@ -0,0 +1,28 @@
+package jw.jzbot.fact.functions.conditional;
+
+import jw.jzbot.fact.ArgumentList;
+import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.Function;
+import jw.jzbot.fact.Sink;
+
+public class IfceqFunction extends Function
+{
+    
+    @Override
+    public void evaluate(Sink sink, ArgumentList arguments, FactContext context)
+    {
+        if (arguments.resolveString(0).equals(arguments.resolveString(1)))
+            arguments.resolve(2, sink);
+        else if (arguments.length() > 3)
+            arguments.resolve(3, sink);
+    }
+    
+    @Override
+    public String getHelp(String topic)
+    {
+        return "Exact same syntax and usage as {ifeq}, but this function is "
+            + "case-sensitive when comparing values to see if they are equal, "
+            + "whereas {ifeq} is case-insensitive.";
+    }
+    
+}
