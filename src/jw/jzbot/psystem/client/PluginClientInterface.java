997f786f51d4641a6450c931cc03deb00ef2ef4a
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-02-26 18:17:49 +0000

    Moved stuff around
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1230 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/psystem/client/PluginClientInterface.java
+++ src/jw/jzbot/psystem/client/PluginClientInterface.java
@@ -1,5 +1,12 @@
 package jw.jzbot.psystem.client;
 
+/**
+ * An interface defining all of the RPC methods that a plugin can call. JZBot itself
+ * implements these methods, and allows them to be called over RPC.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
 public interface PluginClientInterface
 {
     
