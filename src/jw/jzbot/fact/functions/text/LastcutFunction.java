f9e4e0e6f3b076081b37290519defde487f05b01
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-07-13 04:56:21 +0000

    Added the {lastcut} function, which discards all text in a given string after the last instance of a certain substring, if it exists
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1351 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ src/jw/jzbot/fact/functions/text/LastcutFunction.java
@@ -0,0 +1,32 @@
+package jw.jzbot.fact.functions.text;
+
+import jw.jzbot.fact.ArgumentList;
+import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.Function;
+import jw.jzbot.fact.Sink;
+
+public class LastcutFunction extends Function
+{
+    
+    @Override
+    public void evaluate(Sink sink, ArgumentList arguments, FactContext context)
+    {
+        String search = arguments.resolveString(0);
+        String text = arguments.resolveString(1);
+        int index = text.indexOf(search);
+        if(index != -1)
+            text = text.substring(0, index);
+        sink.write(text);
+    }
+    
+    @Override
+    public String getHelp(String topic)
+    {
+        return "Syntax: {lastcut|<search>|<text>} -- Searches for the last location " +
+        		"of <search> in <text> and discards <search> and everything after " +
+        		"it in <text>, then evaluates to the new text. If <search> is not " +
+        		"present, <text> is returned as-is. For example, " +
+        		"{lastcut|.|123.456.789} would evaluate to \"123.456\".";
+    }
+    
+}
