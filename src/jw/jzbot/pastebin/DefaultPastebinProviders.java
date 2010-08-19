1b39d6b1dc83b086f0c31f39401d057c425b78f7
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-08-19 20:56:40 +0000

    Disabled all of the pastebin services that no longer exist. That leaves only two currently in existence. Would anyone else be willing to provide a pastebin that JZBot could use? Tell jcp if you can. Thanks.
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1378 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/pastebin/DefaultPastebinProviders.java
+++ src/jw/jzbot/pastebin/DefaultPastebinProviders.java
@@ -8,13 +8,14 @@ public class DefaultPastebinProviders
     public static void installDefaultSet()
     {
         // FIXME: add paste.icefyre.org
-        PastebinService.installProvider(new PDPastebin("pastebin.com", true,
-                "pastebin.php", true));
+        // PastebinService.installProvider(new PDPastebin("pastebin.com", true,
+        // "pastebin.php", true));
         // p.baf.cc added a captcha, so removed for now.
         // PastebinService.installProvider(new PDPastebin("p.baf.cc", false,
         // "pastebin.php"));
-        PastebinService.installProvider(new PDPastebin("pastebin.flamingspork.com", false,
-                "pastebin.php", true));
+        // PastebinService.installProvider(new PDPastebin("pastebin.flamingspork.com",
+        // false,
+        // "pastebin.php", true));
         // This one is commented out at the request of Amahi
         // PastebinService.installProvider(new PDPastebin("paste.amahi.org", false,
         // "pastebin.php", false));
@@ -22,13 +23,15 @@ public class DefaultPastebinProviders
                 "pastebin.php", true));
         PastebinService.installProvider(new PDPastebin("ampaste.net", true, "pastebin.php",
                 true));
-        PastebinService.installProvider(new PDPastebin("paste.liquidswords.org", false,
-                "pastebin.php", true));
+        // PastebinService.installProvider(new PDPastebin("paste.liquidswords.org", false,
+        // "pastebin.php", true));
         // I haven't checked pastebin.im to see if it supports highlights.
-        PastebinService.installProvider(new PDPastebin("pastebin.im", false, "index.php",
-                false));
-        PastebinService.installProvider(new PDPastebin("ospaste.com", false, "index.php",
-                true));
+        // PastebinService.installProvider(new PDPastebin("pastebin.im", false,
+        // "index.php",
+        // false));
+        // PastebinService.installProvider(new PDPastebin("ospaste.com", false,
+        // "index.php",
+        // true));
         PastebinService.installProvider(new PDPastebin("spartabin.co.cc", false,
                 "pastebin.php", true));
         // PastebinService.installProvider(new PDPastebin("paste.uberdragon.net", false,
