ae35f87b49039254fbf3742b15ce10354a675c01
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-09-03 18:06:20 +0000

    Slight fix to handle restarts properly
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1468 9038e2a8-f00e-11dd-89c5-9df526a60542

--- console.py
+++ console.py
@@ -3,6 +3,7 @@ from java.net import Socket
 from java.io import DataInputStream, DataOutputStream
 import sys
 import jline
+from traceback import print_exc
 
 if len(sys.argv) < 3:
     print "You need to specify the host and port, in that order, to"
@@ -15,20 +16,23 @@ print "Connected! One moment..."
 in_stream = DataInputStream(socket.getInputStream())
 out_stream = DataOutputStream(socket.getOutputStream())
 
-while not socket.isClosed():
-    mode = in_stream.readShort()
-    if mode == 2: # normal text write
-        sys.stdout.write(in_stream.readUTF())
-        sys.stdout.flush()
-    elif mode == 3: # raw_input with prompt
-        result = raw_input(in_stream.readUTF())
-        out_stream.writeUTF(result);
-        out_stream.flush()
-    elif mode == 4: # Exiting
-        break
-    else:
-        print "Invalid mode received: " + str(mode)
-        break
+try:
+    while not socket.isClosed():
+        mode = in_stream.readShort()
+        if mode == 2: # normal text write
+            sys.stdout.write(in_stream.readUTF())
+            sys.stdout.flush()
+        elif mode == 3: # raw_input with prompt
+            result = raw_input(in_stream.readUTF())
+            out_stream.writeUTF(result);
+            out_stream.flush()
+        elif mode == 4: # Exiting
+            break
+        else:
+            print "Invalid mode received: " + str(mode)
+            break
+except:
+    print_exc()
 
 if not socket.isClosed():
     socket.close()
