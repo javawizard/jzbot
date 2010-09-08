4dad01791cc69957c26dd3e3d4514d196fd97e1d
jstoker@jcs.me.uk <jstoker@jcs.me.uk@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-09-08 17:14:55 +0000

    Made the DefaultPastebinProviders.java file slightly more readable.
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1501 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/pastebin/DefaultPastebinProviders.java
+++ src/jw/jzbot/pastebin/DefaultPastebinProviders.java
@@ -7,36 +7,43 @@ public class DefaultPastebinProviders
 {
     public static void installDefaultSet()
     {
-        // FIXME: add paste.icefyre.org
+        // pastebin.com uses different software now.
         // PastebinService.installProvider(new PDPastebin("pastebin.com", true,
         // "pastebin.php", true));
+        //
         // p.baf.cc added a captcha, so removed for now.
         // PastebinService.installProvider(new PDPastebin("p.baf.cc", false,
         // "pastebin.php"));
+        //
         // PastebinService.installProvider(new PDPastebin("pastebin.flamingspork.com",
-        // false,
-        // "pastebin.php", true));
+        // false, "pastebin.php", true));
+        //
         // This one is commented out at the request of Amahi
         // PastebinService.installProvider(new PDPastebin("paste.amahi.org", false,
         // "pastebin.php", false));
+        //
         PastebinService.installProvider(new PDPastebin("pastebin.hu", false,
                 "pastebin.php", true));
+        //
         PastebinService.installProvider(new PDPastebin("ampaste.net", true, "pastebin.php",
                 true));
+        //
         // PastebinService.installProvider(new PDPastebin("paste.liquidswords.org", false,
         // "pastebin.php", true));
+        //
         // I haven't checked pastebin.im to see if it supports highlights.
         // PastebinService.installProvider(new PDPastebin("pastebin.im", false,
-        // "index.php",
-        // false));
+        // "index.php", false));
+        //
         // PastebinService.installProvider(new PDPastebin("ospaste.com", false,
-        // "index.php",
-        // true));
-        PastebinService.installProvider(new PDPastebin("p.jcs.me.uk", false,
-                "pastebin.php", true));
+        // "index.php", true));
+        //
+        PastebinService.installProvider(new PDPastebin("paste.jcs.me.uk", false,
+                "", true));
+        //
         // PastebinService.installProvider(new PDPastebin("paste.uberdragon.net", false,
         // "index.php"));
-        
+        //
         // pastebin.flamingspork.com
         // paste.amahi.org
         // p.baf.cc
