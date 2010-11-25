39908d1a592a1d2e5c55989805d14f4ee148bc3b
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-11-25 06:16:34 +0000

    CPU stats works as expected. W00T!
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1536 9038e2a8-f00e-11dd-89c5-9df526a60542

--- plugins/python/statmonitor.py
+++ plugins/python/statmonitor.py
@@ -5,9 +5,13 @@ statistics of all monitord instances currently connected to the Autobus server.
 dependencies: pyjzbot
 """
 
+import sys
+if "storage/afn-python/src" not in sys.path:
+    sys.path.append("storage/afn-python/src")
 from libautobus import AutobusConnection
 from pyjzbot import makecommand
 import re
+import math
 
 @makecommand
 def monitor(server, channel, pm, sender, source, arguments):
@@ -17,8 +21,43 @@ def monitor(server, channel, pm, sender, source, arguments):
         if interface["name"].startswith("monitor."):
             name = interface["name"][len("monitor."):]
             if "." not in name:
-                stat_machines.append()
-    source.sendMessage("\x02CPU:\x15 \x0302user\x15 \x0311system\x15\x02\x15")
+                stat_machines.append(name)
+    source.sendMessage("\x02CPU:\x0f \x0302user\x0f \x0311system\x0f\x02\x0f "
+            "\x02Memory:\x0f \x0303resident\x0f \x0309buffers/cached\x0f\x02\x0f")
+    if len(stat_machines) == 0:
+        source.sendMessage("No machines currently providing stats.")
+    for machine in stat_machines:
+        stats = bus.get_remote_object_value("monitor." + machine, "status")
+        if stats is None:
+            continue
+        user, system = stats["cpu"]
+        source.sendMessage(create_bar([2, 11], [user, system]) + "   " + machine) 
+
+def create_bar(colors, percents, width=30):
+    """
+    Returns some IRC color-formatted text representing a progress bar
+    with the specified percent values set to the specified colors, both of
+    which should be lists. The percent values can be floats and should range
+    from 0 to 100.
+    """
+    cents_per_bar = 100.0 / width
+    total = 0
+    text = ""
+    for color, percent in zip(colors, percents):
+        bars = percent / cents_per_bar
+        bars = int(bars)
+        if total == 0 and bars == 0: #Guarantee we show at least one colored
+            # vertical bar to indicate the color, since color is significant
+            # in this plugin (blue = processor, green = memory, purple = swap,
+            # red = disk, orange/yellow = network)
+            bars = 1
+        total += bars
+        if total > width:
+            total, bars = width, bars - (total - width)
+        text += "\x03" + str(color).rjust(2, "0") + ("|" * bars)
+    if total < width:
+        text += "\x0300" + ("|" * (width - total))
+    return "[\x02" + text + "\x0f\x02\x0f]"
 
 def init(context):
     global bus
