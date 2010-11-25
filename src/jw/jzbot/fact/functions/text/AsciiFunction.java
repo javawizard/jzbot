3b48daeb37f5e3cf7b9f646cbf3032aafa1b21f4
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-11-25 00:05:34 +0000

    Changed the {ascii} function to allow multiple characters in the input; {ascii|ABC} would evaluate to 65 66 67, for example.
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1534 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/fact/functions/text/AsciiFunction.java
+++ src/jw/jzbot/fact/functions/text/AsciiFunction.java
@@ -1,10 +1,12 @@
 package jw.jzbot.fact.functions.text;
 
+import net.sf.opengroove.common.utils.StringUtils;
 import jw.jzbot.fact.ArgumentList;
 import jw.jzbot.fact.FactContext;
 import jw.jzbot.fact.Function;
 import jw.jzbot.fact.Sink;
 import jw.jzbot.fact.exceptions.FactoidException;
+import jw.jzbot.utils.Utils;
 
 public class AsciiFunction extends Function
 {
@@ -13,22 +15,23 @@ public class AsciiFunction extends Function
     public void evaluate(Sink sink, ArgumentList arguments, FactContext context)
     {
         String s = arguments.resolveString(0);
-        if (s.length() != 1)
-            throw new FactoidException("<char> was \"" + s
-                    + "\", which isn't exactly one character.");
-        sink.write(((int) s.charAt(0)));
+        String[] strs = new String[s.length()];
+        for (int i = 0; i < strs.length; i++)
+            strs[i] = "" + ((int) s.charAt(i));
+        sink.write(StringUtils.delimited(strs, " "));
     }
     
     @Override
     public String getHelp(String topic)
     {
         return "Syntax: {ascii|<char>} -- Evaluates to the numerical code that represents the "
-                + "ascii character <char>. For example, {ascii| } results in \"32\", "
-                + "{ascii|1} results in \"49\", and {ascii|A} results in \"65\". An "
-                + "error will occur if <char> is not a single character.\n"
-                + "TODO: Right now, this function, instead of returning the ascii char value, "
-                + "returns the value of the character in whatever charset you've configured "
-                + "the bot with.";
+            + "ascii character <char>. For example, {ascii| } results in \"32\", "
+            + "{ascii|1} results in \"49\", and {ascii|A} results in \"65\". If "
+            + "<char> contains multiple characters, the result will be the value "
+            + "of each character separated by spaces. "
+            + "TODO: Right now, this function, instead of returning the ascii char value, "
+            + "returns the value of the character in whatever charset you've configured "
+            + "the bot with.";
     }
     
 }
