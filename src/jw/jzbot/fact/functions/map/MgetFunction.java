ddcfb44e627f5d236073daef718635be5edeb5d6
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2011-04-26 22:58:51 +0000

    Changed {mget} to allow a default to be explicitly specified
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1589 9038e2a8-f00e-11dd-89c5-9df526a60542

--- src/jw/jzbot/fact/functions/map/MgetFunction.java
+++ src/jw/jzbot/fact/functions/map/MgetFunction.java
@@ -13,7 +13,9 @@ public class MgetFunction extends Function
     {
         String value =
                 MapUtils.decode(arguments.resolveString(0)).get(arguments.resolveString(1));
-        if (value == null)
+        if (value == null && arguments.length() > 2)
+            value = arguments.resolveString(2);
+        else if (value == null)
             value = "";
         sink.write(value);
     }
@@ -21,10 +23,11 @@ public class MgetFunction extends Function
     @Override
     public String getHelp(String topic)
     {
-        return "Syntax: {mget|<map>|<key>} -- Gets the value of the specified "
-            + "key stored in the specified map, which is a map formerly "
-            + "created with {mcreate}. If the specified key does not exist, "
-            + "the empty string is returned.";
+        return "Syntax: {mget|<map>|<key>|<default>} -- Gets the value of the "
+            + "specified key stored in the specified map, which is a map "
+            + "formerly created with {mcreate}. If the specified key does "
+            + "not exist, <default> will be returned. If <default> is not "
+            + "specified, the empty string will instead be returned.";
     }
     
 }
