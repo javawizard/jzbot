1eaa7a0279a2a11a679add028fed035b13dfdc6d
Alexander Boyd <alex@opengroove.org>
2012-02-27 02:07:32 -0700

    Treat the empty string as false in {if}

--- src/jw/jzbot/fact/functions/conditional/IfFunction.java
+++ src/jw/jzbot/fact/functions/conditional/IfFunction.java
@@ -7,19 +7,16 @@ import jw.jzbot.fact.Function;
 import jw.jzbot.fact.Sink;
 import jw.jzbot.fact.exceptions.FactoidException;
 
-public class IfFunction extends Function
-{
+public class IfFunction extends Function {
     private static final String yesValues = "|y|yes|t|true|1|1.0|";
     private static final String noValues = "|n|no|f|false|0|0.0|";
     
     @Override
-    public void evaluate(Sink sink, ArgumentList arguments, FactContext context)
-    {
+    public void evaluate(Sink sink, ArgumentList arguments, FactContext context) {
         Boolean result = findValueOrNull(arguments.getString(0));
-        if (result == null)
-        {
-            result = findValue(JZBot.evaluateEquation(arguments.getString(0), context
-                    .getChannel()));
+        if (result == null) {
+            result = findValue(JZBot.evaluateEquation(arguments.getString(0),
+                    context.getChannel()));
         }
         if (result)
             arguments.resolve(1, sink);
@@ -27,20 +24,19 @@ public class IfFunction extends Function
             arguments.resolve(2, sink);
     }
     
-    public static boolean findValue(String value)
-    {
+    public static boolean findValue(String value) {
         String toCompare = "|" + value.toLowerCase() + "|";
         if (yesValues.contains(toCompare))
             return true;
-        else if (noValues.contains(toCompare))
+        else if (noValues.contains(toCompare) || value.equals(""))
             return false;
         else
             throw new FactoidException("Value to check was " + toCompare
-                    + " when it should have been in " + yesValues + " or " + noValues);
+                    + " when it should have been in " + yesValues + " or "
+                    + noValues);
     }
     
-    public static Boolean findValueOrNull(String value)
-    {
+    public static Boolean findValueOrNull(String value) {
         String toCompare = "|" + value.toLowerCase() + "|";
         if (yesValues.contains(toCompare))
             return true;
@@ -50,14 +46,12 @@ public class IfFunction extends Function
             return null;
     }
     
-    public String getName()
-    {
+    public String getName() {
         return "if";
     }
     
     @Override
-    public String getHelp(String topic)
-    {
+    public String getHelp(String topic) {
         return "Syntax: {if|<condition>|<trueaction>|<falseaction>} -- Evalutes to "
                 + "<trueaction> if <condition> is one of the \"true\" values, and <falseaction> "
                 + "if <condition> is one of the \"false\" values. If <condition> is neither a "
