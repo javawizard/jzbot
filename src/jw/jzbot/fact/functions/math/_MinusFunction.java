463de2a96c948fd91aa2fbf52767d9034d13ddf4
Alexander Boyd <alex@opengroove.org>
2012-02-27 01:35:59 -0700

    More math functions

--- src/jw/jzbot/fact/functions/math/_MinusFunction.java
+++ src/jw/jzbot/fact/functions/math/_MinusFunction.java
@@ -7,18 +7,19 @@ import jw.jzbot.fact.FactContext;
 import jw.jzbot.fact.Function;
 import jw.jzbot.fact.Sink;
 
-public class _PlusFunction extends Function {
+public class _MinusFunction extends Function {
     
     @Override
     public void evaluate(Sink sink, ArgumentList arguments, FactContext context) {
-        BigDecimal total = new BigDecimal("0");
-        for (int i = 0; i < arguments.length(); i++)
-            total = total.add(new BigDecimal(arguments.resolveString(i)));
+        BigDecimal total = new BigDecimal(arguments.resolveString(0));
+        for (int i = 1; i < arguments.length(); i++)
+            total = total.subtract(new BigDecimal(arguments.resolveString(i)));
         sink.write(total.toString());
+        
     }
     
     @Override
     public String getHelp(String topic) {
-        return "Syntax: {+|<arg1>|<arg2>|...} -- Returns the numerical sum of the specified numbers.";
+        return "Syntax: {-|<arg1>|<arg2>|...} -- Returns the result of subtracting <arg2>, <arg3>, ... from <arg1>.";
     }
 }
--- src/jw/jzbot/fact/functions/math/_StarFunction.java
+++ src/jw/jzbot/fact/functions/math/_StarFunction.java
@@ -7,18 +7,18 @@ import jw.jzbot.fact.FactContext;
 import jw.jzbot.fact.Function;
 import jw.jzbot.fact.Sink;
 
-public class _PlusFunction extends Function {
+public class _StarFunction extends Function {
     
     @Override
     public void evaluate(Sink sink, ArgumentList arguments, FactContext context) {
-        BigDecimal total = new BigDecimal("0");
+        BigDecimal total = new BigDecimal("1");
         for (int i = 0; i < arguments.length(); i++)
-            total = total.add(new BigDecimal(arguments.resolveString(i)));
+            total = total.multiply(new BigDecimal(arguments.resolveString(i)));
         sink.write(total.toString());
     }
     
     @Override
     public String getHelp(String topic) {
-        return "Syntax: {+|<arg1>|<arg2>|...} -- Returns the numerical sum of the specified numbers.";
+        return "Syntax: {*|<arg1>|<arg2>|...} -- Returns the result of multiplying <arg1> by <arg2>, then multiplying the result by <arg3>, and so on.";
     }
 }
