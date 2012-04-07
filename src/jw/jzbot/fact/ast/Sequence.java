571d1eb665c14289b6f053f65121339809990342
Alexander Boyd <alex@opengroove.org>
2012-04-07 01:48:28 -0600

    Getting ready to replace Fact's hand-written recursive descent parser with one that will use Parcon Java

--- src/jw/jzbot/fact/ast/Sequence.java
+++ src/jw/jzbot/fact/ast/Sequence.java
@@ -1,101 +1,98 @@
 package jw.jzbot.fact.ast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import jw.jzbot.fact.FactContext;
 import jw.jzbot.fact.Sink;
 
-public class Sequence extends FactEntity
-{
+public class Sequence extends FactEntity {
     /**
-     * If this sequence is the list of arguments to a function, then this is the function,
-     * and the sequence will not have a parent. Otherwise, this is null and the sequence
-     * will have a parent (unless it's the top-level debug point).
+     * If this sequence is the list of arguments to a function, then this is the
+     * function, and the sequence will not have a parent. Otherwise, this is
+     * null and the sequence will have a parent (unless it's the top-level debug
+     * point).
      */
     private FunctionReference function;
     
-    public FunctionReference getFunction()
-    {
+    public FunctionReference getFunction() {
         return function;
     }
     
     /**
-     * Sets the function reference for this sequence, and changes all children of this
-     * sequence to have the specified function reference as their parent.
+     * Sets the function reference for this sequence, and changes all children
+     * of this sequence to have the specified function reference as their
+     * parent.
      * 
      * @param function
      */
-    public void setFunction(FunctionReference function)
-    {
+    public void setFunction(FunctionReference function) {
         this.function = function;
-        for (FactEntity child : list)
-        {
+        for (FactEntity child : list) {
             child.setParent(function);
         }
     }
     
     private ArrayList<FactEntity> list = new ArrayList<FactEntity>();
     
-    public int length()
-    {
+    public Sequence() {
+    }
+    
+    public Sequence(List<FactEntity> list) {
+        this.list.addAll(list);
+    }
+    
+    public int length() {
         return list.size();
     }
     
-    public FactEntity get(int index)
-    {
+    public FactEntity get(int index) {
         return list.get(index);
     }
     
     /**
-     * Adds the specified item to this sequence and sets the parent of the specified item
-     * to this sequence.
+     * Adds the specified item to this sequence and sets the parent of the
+     * specified item to this sequence.
      * 
      * @param entity
      */
-    public void add(FactEntity entity)
-    {
+    public void add(FactEntity entity) {
         list.add(entity);
     }
     
     /**
-     * Resolves all of the children of this sequence, concatenates their output together,
-     * and returns it.
+     * Resolves all of the children of this sequence, concatenates their output
+     * together, and returns it.
      */
     @Override
-    public void execute(Sink sink, FactContext context)
-    {
+    public void execute(Sink sink, FactContext context) {
         /*
-         * Due to the fact that the parser wraps a lot of single entities with a sequence,
-         * this makes everything run faster if we check for sequences that have only one
-         * element in them.
+         * Due to the fact that the parser wraps a lot of single entities with a
+         * sequence, this makes everything run faster if we check for sequences
+         * that have only one element in them.
          */
-        for (FactEntity entity : list)
-        {
+        for (FactEntity entity : list) {
             entity.resolve(sink, context);
         }
     }
     
     @Override
-    public void explain(Sink sink, int indentation, int increment)
-    {
+    public void explain(Sink sink, int indentation, int increment) {
         explain(sink, indentation, increment, true);
     }
     
-    public void explain(Sink sink, int indentation, int increment, boolean prefix)
-    {
-        if (prefix)
-        {
+    public void explain(Sink sink, int indentation, int increment,
+            boolean prefix) {
+        if (prefix) {
             sink.write(spaces(indentation));
             sink.write("sequence:\n");
         }
-        for (FactEntity entity : list)
-        {
+        for (FactEntity entity : list) {
             entity.explain(sink, indentation + increment, increment);
         }
     }
     
-    public void remove(int index)
-    {
+    public void remove(int index) {
         list.remove(index);
     }
 }
