2d057cf0dbe4b927e64f23ba88770b350bffb815
Alexander Boyd <alex@opengroove.org>
2015-04-21 10:21:12 -0600

    More vault stuff

--- src/jw/jzbot/fact/functions/vault/SvdenyFunction.java
+++ src/jw/jzbot/fact/functions/vault/SvdenyFunction.java
@@ -1,7 +1,21 @@
 package jw.jzbot.fact.functions.vault;
 
+import jw.jzbot.fact.ArgumentList;
+import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.Function;
+import jw.jzbot.fact.Sink;
+
 /**
  * Created by aboyd on 2015-04-09.
  */
-public class SvdenyFunction {
+public class SvdenyFunction extends Function {
+    @Override
+    public void evaluate(Sink sink, ArgumentList arguments, FactContext context) {
+
+    }
+
+    @Override
+    public String getHelp(String topic) {
+        return "TBD";
+    }
 }
--- src/jw/jzbot/fact/functions/vault/SvlistFunction.java
+++ src/jw/jzbot/fact/functions/vault/SvlistFunction.java
@@ -1,7 +1,21 @@
 package jw.jzbot.fact.functions.vault;
 
+import jw.jzbot.fact.ArgumentList;
+import jw.jzbot.fact.FactContext;
+import jw.jzbot.fact.Function;
+import jw.jzbot.fact.Sink;
+
 /**
  * Created by aboyd on 2015-04-07.
  */
-public class SvlistFunction {
+public class SvlistFunction extends Function {
+    @Override
+    public void evaluate(Sink sink, ArgumentList arguments, FactContext context) {
+
+    }
+
+    @Override
+    public String getHelp(String topic) {
+        return "";
+    }
 }
