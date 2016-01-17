a2a1dabdca3be76b2d3974159968e88f550c32b1
Alexander Boyd <alex@opengroove.org>
2016-01-17 01:25:25 -0700

    Allow the {if} function to contain an unlimited number of conditional clauses, so that it can now be used as either {if|<condition>|<action>|<condition>|<action>|...|<otherwise>} or {if|<condition>|<action>|<condition>|<action>|...}, much like the {switch} function

--- src/jw/jzbot/fact/functions/conditional/IfFunction.java
+++ src/jw/jzbot/fact/functions/conditional/IfFunction.java
@@ -10,20 +10,24 @@ import jw.jzbot.fact.exceptions.FactoidException;
 public class IfFunction extends Function {
     private static final String yesValues = "|y|yes|t|true|1|1.0|";
     private static final String noValues = "|n|no|f|false|0|0.0|";
-    
+
     @Override
     public void evaluate(Sink sink, ArgumentList arguments, FactContext context) {
-        Boolean result = findValueOrNull(arguments.getString(0));
-        if (result == null) {
-            result = findValue(JZBot.evaluateEquation(arguments.getString(0),
-                    context.getChannel()));
+        for (int i = 0; i < arguments.length() - 1; i += 2) {
+            Boolean result = findValueOrNull(arguments.getString(i));
+            if (result == null) {
+                result = findValue(JZBot.evaluateEquation(arguments.getString(i),
+                        context.getChannel()));
+            }
+            if (result) {
+                arguments.resolve(i + 1, sink);
+                return;
+            }
         }
-        if (result)
-            arguments.resolve(1, sink);
-        else if (arguments.length() > 2)
-            arguments.resolve(2, sink);
+        if ((arguments.length() % 2) == 1)
+            arguments.resolve(arguments.length() - 1, sink);
     }
-    
+
     public static boolean findValue(String value) {
         String toCompare = "|" + value.toLowerCase() + "|";
         if (yesValues.contains(toCompare))
@@ -35,7 +39,7 @@ public class IfFunction extends Function {
                     + " when it should have been in " + yesValues + " or "
                     + noValues);
     }
-    
+
     public static Boolean findValueOrNull(String value) {
         String toCompare = "|" + value.toLowerCase() + "|";
         if (yesValues.contains(toCompare))
@@ -45,21 +49,24 @@ public class IfFunction extends Function {
         else
             return null;
     }
-    
+
     public String getName() {
         return "if";
     }
-    
+
     @Override
     public String getHelp(String topic) {
-        return "Syntax: {if|<condition>|<trueaction>|<falseaction>} -- Evalutes to "
-                + "<trueaction> if <condition> is one of the \"true\" values, and <falseaction> "
+        return "Syntax: {if|<condition>|<action>|...} or "
+                + "{if|<condition>|<action>|...|<otherwise>} -- Evalutes to "
+                + "the first <action> for which the preceding <condition> is one "
+                + "of the \"true\" values, and <otherwise> "
                 + "if <condition> is one of the \"false\" values. If <condition> is neither a "
                 + "true value nor a false value, it is interpreted as an equation (as if "
                 + "it were passed to {eval}), and then compared again.\n"
                 + "The true values are y, yes, t, true, 1, and 1.0, and the false values "
                 + "are n, no, f, false, 0, and 0.0. These values are case-insensitive. "
-                + "<falseaction> is also optional.";
+                + "<otherwise> is also optional, and defaults to the empty string if only an "
+                + "even number of arguments are given.";
     }
-    
+
 }
